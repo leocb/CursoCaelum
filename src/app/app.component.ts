74f7e398d020b38dbc560c53b705803c2300eb0a
Leo Bottaro <leobottaro@gmail.com>
2017-06-19 12:10:56 -0300

    Adicionado atributos ao component fotos, bootstrap.css instalado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FotoModule } from './foto/foto.module';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Bem-vindo ao CaelumPic!';
 }
---
+++ src/app/foto/foto.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+@Component({
+    selector: 'foto',
+    templateUrl: './foto.component.html'
+})
+export class FotoComponent {
+    @Input() url;
+    @Input() title
+}
